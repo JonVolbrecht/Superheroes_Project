490bcf27a9bbc3f7dc0964fa8f88837e9066118d
JonVolbrecht <jonathanvolbrecht@yahoo.com>
2021-03-17 10:37:59 -0500

    View added to superhero controller and ApplicationDbContext added to controller as well

--- Superheroes/Controllers/SuperHeroController.cs
+++ Superheroes/Controllers/SuperHeroController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Superheroes.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,15 @@ namespace Superheroes.Controllers
 {
     public class SuperHeroController : Controller
     {
+        public object ApplicationDbContext { get; }
+
+        
+
+        public SuperHeroController()
+        {
+            ApplicationDbContext = ApplicationDbContext;
+        }
+
         // GET: SuperHeroController
         public ActionResult Index()
         {
